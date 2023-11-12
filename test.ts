04ae62ca5c3515ad06020b3cf57dc041c6914653
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2023-11-12 08:00:21 +0000

    updated tests; TODO add transpose to playTune()

--- test.ts
+++ test.ts
@@ -1,28 +1,33 @@
 // *********** test codes **********
-/************** 
 // perform a built-in FlexFX with all the defaults
-flexFX.playTune("birthday","violin",true,100,5000);
-pause(1000);
+basic.showIcon(IconNames.Target);
+flexFX.playTune("birthday","whale");
 
+basic.showIcon(IconNames.Yes);
+pause(2000);
 
 // perform the simple built-in chime flexFX
+basic.showIcon(IconNames.Target);
 flexFX.playFlexFX("ting", true, Note.G5, 180, 400); // up a fifth
 flexFX.playFlexFX("ting", true, Note.E5, 180, 400); // up a major 3rd
 flexFX.playFlexFX("ting", true, Note.C5, 250, 1600);
-pause(1000);
 
+basic.showIcon(IconNames.Yes);
+pause(2000);
 
 // perform like a cat
+basic.showIcon(IconNames.Target);
 flexFX.playFlexFX("miaow", true, 900, 255, 1000);
-pause(300);
+pause(800);
 flexFX.playFlexFX("miaow", true, 1100, 255, 500);
 pause(300);
 flexFX.playFlexFX("miaow", true, 800, 255, 1500);
 
-pause(1000);
+basic.showIcon(IconNames.Yes);
+pause(2000);
 
-
-// perform "New World" theme on the 2-part horn flexFX
+// perform "New World" theme on the 2-part horn as single notes
+basic.showIcon(IconNames.Target);
 flexFX.playFlexFX("horn", true, Note.E3, 255, 900);
 flexFX.playFlexFX("horn", true, Note.G3, 255, 300);
 flexFX.playFlexFX("horn", true, Note.G3, 255, 1200);
@@ -35,14 +40,15 @@ flexFX.playFlexFX("horn", true, Note.G3, 255, 600);
 flexFX.playFlexFX("horn", true, Note.E3, 255, 600);
 flexFX.playFlexFX("horn", true, Note.D3, 255, 2400);
 
-pause(1000);
-***********/
+basic.showIcon(IconNames.Yes);
+pause(2000);
+
 // create a flexFX for a two-tone police-siren (middle part is silent)
+basic.showIcon(IconNames.Target);
 flexFX.defineFlexFX("police", 760, 160, flexFX.Wave.Sawtooth, flexFX.Attack.Even, flexFX.Effect.None, 800, 200, 450);
 // (add a silent gap in the middle)
 flexFX.extendFlexFX("police", flexFX.Wave.Silence, flexFX.Attack.Even, flexFX.Effect.None, 560, 200, 100);
 flexFX.extendFlexFX("police", flexFX.Wave.Sawtooth, flexFX.Attack.Even, flexFX.Effect.None, 600, 160, 450);
-
 // queue-up a sequence of Plays on the Play-list (complete with Doppler-shift)
 flexFX.playFlexFX("police", false, 800, 16, 1000);
 flexFX.playFlexFX("police", false, 800, 32, 1000);
@@ -54,17 +60,19 @@ flexFX.playFlexFX("police", false, 750, 128, 1000);
 flexFX.playFlexFX("police", false, 750, 64, 1000);
 flexFX.playFlexFX("police", false, 750, 32, 1000);
 flexFX.playFlexFX("police", false, 750, 16, 1000);
-
+flexFX.startPlaying(); // kick off the Play-list
 // while the Play-list is playing, flash the blue light (sort of)
-flexFX.startPlaying();
 while (flexFX.isActive()) {
     basic.showIcon(IconNames.SmallDiamond);
     basic.showIcon(IconNames.Diamond);
 }
-/***************** 
-pause(1000);
+
+basic.showIcon(IconNames.Yes);
+pause(2000);
 
 // queue-up a sequence of Plays on the Play-list (J.S.Bach)
+basic.showIcon(IconNames.Target);
+flexFX.stopPlaying();  // don't start Playing yet...
 flexFX.playFlexFX("violin", false, Note.E5, 250, 300);
 flexFX.playFlexFX("violin", false, Note.A5, 250, 900);
 flexFX.playFlexFX("violin", false, Note.E5, 250, 300);
@@ -80,7 +88,7 @@ flexFX.playFlexFX("violin", false, Note.B4, 250, 150);
 flexFX.playFlexFX("violin", false, Note.D5, 250, 150);
 flexFX.playFlexFX("violin", false, Note.C5, 250, 300);
 flexFX.playFlexFX("violin", false, Note.A4, 250, 900);
-
+flexFX.startPlaying(); // kick off the Play-list
 // while the Play-list is playing, jiggle a note around
 while (flexFX.isActive()) {
     images.iconImage(IconNames.QuarterNote).showImage(-2, 150);
@@ -89,11 +97,12 @@ while (flexFX.isActive()) {
     images.iconImage(IconNames.QuarterNote).showImage(-1, 150);
 }
 
+basic.showIcon(IconNames.Yes);
+pause(2000);
+
+// use events to choreograph faces to sounds
 basic.showIcon(IconNames.Target);
-pause(1000);
-
-
-// queue up some Plays on the Play-list, with pauses queued in-between
+// first queue up some Plays on the Play-list, with pauses queued in-between
 flexFX.stopPlaying();  // don't start Playing yet...
 flexFX.playFlexFX("cry", false, 200, 250, 1000);
 flexFX.playSilence(2000);
@@ -106,7 +115,6 @@ flexFX.playSilence(800);
 flexFX.playFlexFX("cry", false, 800, 250, 1000);
 basic.showNumber(flexFX.waitingToPlay());
 pause(500);
-// use events to choreograph faces to sounds
 basic.showIcon(IconNames.Sad);
 pause(1000)
 flexFX.startPlaying(); // kick off the Play-list
@@ -117,12 +125,13 @@ while (flexFX.isActive()) {
     basic.showIcon(IconNames.Sad); // close the mouth again
     // (the active Player now "Plays" the queued silence)
 }
-pause(500);
-basic.showIcon(IconNames.Happy);
 
-pause(2000)
-// now re-build the Play-list of 5 cries
-basic.showIcon(IconNames.Sad);
+basic.showIcon(IconNames.Yes);
+pause(2000);
+
+// synchronise a different way, by playing queued Plays one-at-a-time with explicit pauses...
+basic.showIcon(IconNames.Target);
+// first re-build the Play-list of 5 cries
 flexFX.stopPlaying();  // inhibit Playing
 flexFX.playFlexFX("cry", false, 200, 250, 1000);
 flexFX.playFlexFX("cry", false, 300, 250, 1000);
@@ -130,11 +139,9 @@ flexFX.playFlexFX("cry", false, 400, 250, 1000);
 flexFX.playFlexFX("cry", false, 600, 250, 1000);
 flexFX.playFlexFX("cry", false, 800, 250, 1000);
 basic.showNumber(flexFX.waitingToPlay());
-
 pause(1000)
-// synchronise a different way, by playing queued Plays one-at-a-time, 
-// with explicit pauses...
-let delay = 1600;
+basic.showIcon(IconNames.Sad);
+let delay = 3000;
 while (flexFX.waitingToPlay() > 0) {
     flexFX.startPlaying(); // allow the next Play to happen
     pause(20);
@@ -144,14 +151,15 @@ while (flexFX.waitingToPlay() > 0) {
     flexFX.awaitPlayFinish();
     basic.showIcon(IconNames.Sad); // close the mouth again
     pause(delay);
-    delay -= 200; // decrease the silence in-between 
+    delay -= 600; // decrease the silence in-between 
 }
-
 pause(500);
-basic.showIcon(IconNames.Happy);
-pause(2000)
+
+basic.showIcon(IconNames.Yes);
+pause(2000);
 
 // check we can delete an unwanted Play-list of 5 tweets
+basic.showIcon(IconNames.Target);
 flexFX.stopPlaying();  // inhibit Playing
 flexFX.playFlexFX("tweet", false, 200, 250, 1000);
 flexFX.playFlexFX("tweet", false, 300, 250, 1000);
@@ -161,17 +169,42 @@ flexFX.playFlexFX("tweet", false, 800, 250, 1000);
 basic.showNumber(flexFX.waitingToPlay());
 pause(1000);
 basic.showIcon(IconNames.No);
-pause(1000);
 flexFX.deletePlaylist();
 basic.showNumber(flexFX.waitingToPlay());
+
+basic.showIcon(IconNames.Yes);
 pause(2000);
 
 // check playing of tunes
-flexFX.playTune("birthday", "ting");
-flexFX.playTune("newWorld", "horn",true,100,);
-flexFX.playTune("birthday", "woof",);
+basic.showIcon(IconNames.Target);
+flexFX.playTune("birthday", "tweet");
 
-// compose a new tune
+basic.showIcon(IconNames.Yes);
+pause(2000);
+
+// check asynchrony of tunes
+basic.showIcon(IconNames.Target);
+flexFX.playTune("birthday", "woof",false);
+flexFX.startPlaying();
+while (flexFX.isActive()) {
+    images.iconImage(IconNames.StickFigure).showImage(-1, 150);
+    images.iconImage(IconNames.StickFigure).showImage(0, 150);
+    images.iconImage(IconNames.StickFigure).showImage(1, 150);
+    images.iconImage(IconNames.StickFigure).showImage(0, 150);
+}
+
+basic.showIcon(IconNames.Yes);
+pause(2000);
+
+// check volume/duration overrides
+basic.showIcon(IconNames.Target);
+flexFX.playTune("newWorld", "horn",true,100,20000);
+
+basic.showIcon(IconNames.Yes);
+pause(2000);
+
+// compose a brand new tune
+basic.showIcon(IconNames.Target);
 flexFX.composeTune("edelweiss", "4E4 2G4 6D5 4C5 2G4 6F4");
 flexFX.extendTune("edelweiss", "4E4 2E4 2E4 2F4 2G4 6A4 6G4");
 flexFX.extendTune("edelweiss", "4E4 2G4 6D5 4C5 2G4 6F4");
@@ -181,8 +214,10 @@ flexFX.extendTune("edelweiss", "4A4 2C5 4D5 2C5 6B4 6G4");
 flexFX.extendTune("edelweiss", "4E4 2G4 6D5 4C5 2G#4 6F5");
 flexFX.extendTune("edelweiss", "4E5 2G4 2G4 2A4 2B4 6C5 6C5");
 
-// Play it asynchronously on the flute, lasting a minute
-flexFX.playTune("edelweiss","flute",false, 150, 60000);
+// Play it asynchronously on the chime, lasting a full minute
+flexFX.startPlaying();
+flexFX.playTune("edelweiss","chime",false, 150, 60000);
+
 // while the Play-list is playing, keep rotating a flower!
 while (flexFX.isActive()) {
     basic.showLeds(`
@@ -218,4 +253,4 @@ while (flexFX.isActive()) {
         `);
     basic.pause(30);
 }
-**************/
+basic.showIcon(IconNames.Yes);
